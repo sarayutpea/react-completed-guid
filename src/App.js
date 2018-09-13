7af4b3aef8e8a0fa7d0b3460c10dcdf9b8bd2b24
PEA <sarayut.jansoda@hotmail.com>
2018-09-13 14:46:36 +0700

    Hi

--- src/App.js
+++ src/App.js
@@ -1,53 +1,69 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 import Person from './Person/Person';
 
-
 class App extends Component {
-  state = { 
+  state = {
     persons: [
-      { name: "pae", age: 25 },
-      { name: "oor", age: 25 },
-      { name: "stable", age: 26 }
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
     ],
-    otherstate: "some other value"
+    otherState: 'some other value'
   }
 
-  switchNameHandler = (event)=>{
-    // console.log('was click');
-    this.setState({
+  switchNameHandler = (newName) => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState( {
       persons: [
-        { name: "newName", age: 28 },
-        { name: (event.target.value ? event.target.value : ``) , age: 29 },
-        { name: (event.target.value ? event.target.value : ``), age: 26 }
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 27 }
       ]
-    })
+    } )
   }
 
-  render() {
+  nameChangedHandler = (event) => {
+    this.setState( {
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    } )
+  }
+
+  render () {
     const style = {
-      backgroundColor: `white`,
-      borderRadius: `10px`,
-      font: `inherlit`,
-      border: `1px sloid`,
-      padding: `8px`,
-      cursor: `pointer`
-    }
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
 
     return (
       <div className="App">
-        <h1> Hello I'm Pae</h1>
-        <h3>it is really working!</h3>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
         <button 
-          style={ style }
-          onClick={ this.switchNameHandler.bind(this, "biginner") }>Switch name</button>
-        <Person name={ this.state.persons[0].name } age={ this.state.persons[0].age } />
-        <Person click={ this.switchNameHandler.bind(this, "foreigners") } changed={ this.switchNameHandler } name={ this.state.persons[1].name } age={ this.state.persons[1].age } />
-        <Person name={ this.state.persons[2].name } age={ this.state.persons[2].age } />
+          style={style}
+          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button>
+        <Person 
+          name={this.state.persons[0].name} 
+          age={this.state.persons[0].age} />
+        <Person 
+          name={this.state.persons[1].name} 
+          age={this.state.persons[1].age}
+          click={this.switchNameHandler.bind(this, 'Max!')}
+          changed={this.nameChangedHandler} >My Hobbies: Racing</Person>
+        <Person 
+          name={this.state.persons[2].name} 
+          age={this.state.persons[2].age} />
       </div>
     );
-    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, `Does this work now`));
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,15 @@
 import React from 'react';
+
 import './Person.css';
 
-const person = (props) => (
-      <div className="person">
-          <p onClick={ props.click }>I'm { props.name } age: { props.age }</p>
-          <p>{ props.children }</p>
-          <input type="text" onChange={ props.changed } />
+const person = ( props ) => {
+    return (
+        <div className="Person">
+            <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
+            <p>{props.children}</p>
+            <input type="text" onChange={props.changed} value={props.name} />
         </div>
     )
+};
+
 export default person;
